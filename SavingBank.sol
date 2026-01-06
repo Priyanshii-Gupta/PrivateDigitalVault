5ef3d753f0294783a52b0e7b455bd1a8438d9ac4
Priyanshii-Gupta <pg3162726@gmail.com>
2026-01-06 20:08:02 +0530

    PrivateDigitalVault

--- SavingBank.sol
+++ SavingBank.sol
@@ -2,33 +2,31 @@
 
 pragma solidity >=0.8.2 < 0.9;
 
-contract PersonalSavingsBank {
-// Mapping to store balance of each customer (address)
-mapping(address =>uint) private balances;
+contract PrivateVault {
+    address public owner=msg.sender;
 
 //Event logs
-event Deposit(address indexed account, uint amount);
+event Deposited(address indexed account, uint amount);
 event Withdraw(address indexed account, uint amount);
 
 //Deposit money into your account
 function deposit() public payable{
 require(msg.value > 0, "Deposit amount must be greater than 0");
-balances[msg.sender] += msg.value;
 emit Deposit(msg.sender, msg.value); 
 }
 
+
+// Shows how much ETH the vault currently has
+function getBalance() public view returns (uint256) {
+        return address(this).balance
+}
+
+
 // Withdraw Money from ur account 
 function withdraw(uint amount) public {
-    require(amount>0, "Withdraw amount must be grater than 0");
-    require(balances[msg.sender] >= amount, "Insufficient balance");
-    balances[msg.sender] -= amount;
-    payable(msg.sender).transfer(amount);
-    emit Withdraw(msg.sender,amount);
-}
-
-//Show your account balance
-function getBalance() public view returns (uint256) {
-    return balances[msg.sender];
+require(msg.sender == owner, "Only owner can withdraw");  
+require(amount <= address(this).balance, "Insufficient contract balance");
+ payable(owner).transfer(amount);
+emit Withdraw(owner, amount);
 }
 }
-
